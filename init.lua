3521722254c69b1d5757e04590c7c7654c8abeab
Michael Mogenson <900731+mogenson@users.noreply.github.com>
2025-11-27 20:27:21 -0500

    Bump version to 1.0
                                       .''.       
           .''.      .        *''*    :_\/_:     . 
          :_\/_:   _\(/_  .:.*_\/_*   : /\ :  .'.:.'.
      .''.: /\ :   ./)\   ':'* /\ * :  '..'.  -=:o:=-
     :_\/_:'.:::.    ' *''*    * '.\'/.' _\(/_'.':'.'
     : /\ : :::::     *_\/_*     -= o =-  /)\    '  *
      '..'  ':::'     * /\ *     .'/.\'.   '
          *            *..*         :

--- init.lua
+++ init.lua
@@ -46,7 +46,7 @@ PaperWM.__index = PaperWM
 
 -- Metadata
 PaperWM.name = "PaperWM"
-PaperWM.version = "1.0-beta-1"
+PaperWM.version = "1.0"
 PaperWM.author = "Michael Mogenson"
 PaperWM.homepage = "https://github.com/mogenson/PaperWM.spoon"
 PaperWM.license = "MIT - https://opensource.org/licenses/MIT"
